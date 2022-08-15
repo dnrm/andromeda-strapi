f21366ab3944cb360756ea4850b2bbe09dfae173
Daniel Medina <daniel@medina.com>
2022-08-14 19:35:44 -0500

    :sparkles: Added cache congif

--- config/plugins.js
+++ config/plugins.js
@@ -14,4 +14,21 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  "rest-cache": {
+    config: {
+      provider: {
+        name: "memory",
+        options: {
+          max: 32767,
+          maxAge: 600,
+        },
+      },
+      strategy: {
+        contentTypes: [
+          // list of Content-Types UID to cache
+          "api::product.product"
+        ],
+      },
+    },
+  },
 });
